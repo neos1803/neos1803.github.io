bb451aa2b6fda9d9505b06d849b17f71ef9e5ef0
Nanda Purba <oliviergiroud1998@gmail.com>
2020-09-23 03:14:23 +0700

    Change landing page

---
+++ js/porto.js
@@ -0,0 +1,11 @@
+function introFunc() {
+    document.getElementById("introSection").classList.toggle("show");
+}
+
+function portoFunc() {
+    document.getElementById("portoSection").classList.toggle("show");
+}
+
+function introFunc() {
+    document.getElementById("introSection").classList.toggle("show");
+}
